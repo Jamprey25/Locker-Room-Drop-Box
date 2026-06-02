9f52b592fa01a6721bfefcfbf636fb65fa9a2653
Jamprey25 <53832941+Jamprey25@users.noreply.github.com>
2026-06-02 14:47:53 -0400

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AppSessionProvider } from "@/components/providers/app-session-provider";
 import { ToastProvider } from "@/components/ui/toast";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
